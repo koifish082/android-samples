db03f11a2860c3755892d7710cd7e79a4d2a27ee
koifish082 <ganbare.carp@gmail.com>
2018-10-20 11:59:26 +0900

    Update kotlin version and add INTERNET permission to gradle file

--- app/src/main/java/koifish082/android/sample/com/api/androidsamples/presentation/di/module/RepositoryModule.kt
+++ app/src/main/java/koifish082/android/sample/com/api/androidsamples/presentation/di/module/RepositoryModule.kt
@@ -3,7 +3,6 @@ package koifish082.android.sample.com.api.androidsamples.presentation.di.module
 import dagger.Module
 import dagger.Provides
 import koifish082.android.sample.com.api.androidsamples.presentation.presenter.RepositorySearchPresenter
-import javax.inject.Singleton
 
 @Module
 class RepositoryModule {
