4cbff3a653e552ad0d9cf27ff9f5a5471a94f1bd
Kazuhiro Ike <ganbare.carp@gmail.com>
2018-11-02 14:44:44 +0900

    Set release build setting to gradle

--- app/src/main/java/koifish082/android/sample/com/api/androidsamples/data/repository/BaseGithubApiRepository.kt
+++ app/src/main/java/koifish082/android/sample/com/api/androidsamples/data/repository/BaseGithubApiRepository.kt
@@ -6,11 +6,9 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 abstract class BaseGithubApiRepository {
-
-        protected val retrofit: Retrofit = Retrofit.Builder()
-                .baseUrl(BaseRequest.baseUrl)
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
+    protected val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BaseRequest.baseUrl)
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 }
