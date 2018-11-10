c9216326a805911c32780e58dfa5001f4f388b88
Kazuhiro Ike <ganbare.carp@gmail.com>
2018-11-10 14:05:01 +0900

    Change directory name to correspond to the new package name and update tareget sdk version to the latest

--- app/src/main/java/com/koifish/android/sample/domain/entity/Result.kt
+++
@@ -1,9 +0,0 @@
-package com.koifish082.android.samples.domain.entity
-
-class Result<T> {
-
-    var success: T? = null
-
-    var failure: Error? = null
-
-}
---
+++ app/src/main/java/com/koifish082/android/samples/presentation/view/adapter/RepositoryAdapter.kt
@@ -0,0 +1,12 @@
+package com.koifish082.android.sample.presentation.view.adapter
+
+import android.support.v7.widget.RecyclerView
+
+class RepositoryAdapter : RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder> {
+    
+    companion object {
+        class RepositoryViewHolder {
+
+        }
+    }
+}
